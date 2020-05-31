12738bee7ce0c5a2983ee9288890f756f2f44000
NationalNight <904095866@qq.com>
2020-05-31 14:14:47 +0800

    add Loss weight

--- train.py
+++ train.py
@@ -78,6 +78,39 @@ def KMeanupdate(encode_record, params):
     return tf.constant(weights, dtype=tf.float32)
 
 
+def normalize(weight):
+    return weight / np.mean(weight)
+
+
+def Onlylossupdate(old_loss, loss, params):
+    loss_weight = np.array(loss)
+    # loss_weight = normalize(loss_weight)
+    loss_weight = np.power(loss_weight, params.alpha)
+    loss_weight = normalize(loss_weight)
+    return tf.constant(loss_weight, dtype=tf.float32)
+
+
+def Trendlossupdate(old_loss, loss, params):
+    loss_weight = np.array(loss)
+    old_loss_weight = np.array(old_loss)
+    trend_weight = np.abs(old_loss_weight - loss_weight) / loss_weight
+    trend_weight = normalize(trend_weight)
+    loss_weight = normalize(loss_weight)
+    loss_weight = loss_weight * trend_weight
+    loss_weight = normalize(loss_weight)
+    loss_weight = np.power(loss_weight, params.alpha)
+    loss_weight = normalize(loss_weight)
+    return tf.constant(loss_weight, dtype=tf.float32)
+
+
+def FeatureL1update(old_encode_record, encode_record, params):
+    diff = [np.abs(old_encode_record[i] - encode_record[i]) for i in range(len(encode_record))]
+    loss_weight = [np.mean(diff[i]) for i in range(len(diff))]
+    loss_weight = normalize(loss_weight)
+    loss_weight = np.power(loss_weight, params.alpha)
+    loss_weight = normalize(loss_weight)
+    return tf.constant(loss_weight, dtype=tf.float32)
+
 def trainNeighbourWeight(params, weightUpdateFunc):
     dataloader = data.DataLoader(params)
     trainSet, testSet, valSet = dataloader.readData()
@@ -155,6 +188,173 @@ def trainNeighbourWeight(params, weightUpdateFunc):
     print("%.3lf" % acc[9])
 
 
+def trainLossWeight(params, weightUpdateFunc):
+    dataloader = data.DataLoader(params)
+    trainSet, testSet, valSet = dataloader.readData()
+
+    splitModel = model.makeSplitModel(params)
+
+    optimizer = keras.optimizers.Adam(lr=params.learningRate)
+    loss_fn = keras.losses.CategoricalCrossentropy()
+    loss_fn_2 = keras.losses.categorical_crossentropy
+    metricAcc = keras.metrics.CategoricalAccuracy()
+    cateAcc = model.CateAcc()
+
+    datalen = dataloader.trainLen
+    weights = tf.ones([datalen], dtype=tf.float32)
+    timer = utils.Timer()
+    # trainables = [splitModel.conv.trainable_variables,
+    #   splitModel.predict.trainable_variables]
+    for epochID in range(1, params.numEpochs+1):
+        IDcnt = 0
+        batchCnt = 0
+        metricAcc.reset_states()
+        trainloss = 0
+        # encode_record = []
+        loss_record = []
+        for batch, labels in trainSet:
+            batchSize = batch.shape[0]
+            batchWeight = weights[IDcnt: IDcnt+batchSize]
+            with tf.GradientTape() as tape:
+                prediction, encoding = splitModel(batch, training=True)
+                loss = loss_fn(labels, prediction, batchWeight)
+                loss_vec = loss_fn_2(labels, prediction)
+            trainloss += loss
+            gradient = tape.gradient(loss, splitModel.trainable_variables)
+            optimizer.apply_gradients(
+                zip(gradient, splitModel.trainable_variables))
+            metricAcc.update_state(labels, prediction)
+
+            # encode_record.append(encoding.numpy())
+            loss_record.append(loss_vec.numpy())
+
+            batchCnt += 1
+            IDcnt += batchSize
+        trainAcc = metricAcc.result()
+        trainloss /= batchCnt
+
+        batchCnt = 0
+        testloss = 0
+        metricAcc.reset_states()
+        for batch, labels in testSet:
+            prediction, _ = splitModel(batch, training=False)
+            loss = loss_fn(labels, prediction)
+            testloss += loss
+            batchCnt += 1
+            metricAcc.update_state(labels, prediction)
+        testAcc = metricAcc.result()
+        testloss /= batchCnt
+
+        print(f"Epoch {epochID} {timer()}")
+        print(f"Train Loss: {trainloss} Accuracy: {trainAcc}")
+        print(f"Test Loss: {testloss} Accuracy: {testAcc}")
+
+        if epochID >= params.warmUpEpochs:
+            loss_record = np.concatenate(loss_record)
+            weights = weightUpdateFunc(old_loss_record, loss_record, params)
+            print(weights)
+
+        old_loss_record = loss_record
+
+    splitModel.save_weights(
+        path.join(params.modelPath, "splitModel.keras"))
+    metricAcc.reset_states()
+    cateAcc.reset_states()
+    for batch, labels in valSet:
+        prediction, _ = splitModel(batch, training=False)
+        metricAcc.update_state(labels, prediction)
+        cateAcc.update_state(labels, prediction)
+    acc = cateAcc.result()
+    print(metricAcc.result())
+    for i in acc:
+        print("%.2lf," % i, end=' ')
+    print()
+    print("%.3lf" % acc[9])
+
+
+def trainFeatureWeight(params, weightUpdateFunc):
+    dataloader = data.DataLoader(params)
+    trainSet, testSet, valSet = dataloader.readData()
+
+    splitModel = model.makeSplitModel(params)
+
+    optimizer = keras.optimizers.Adam(lr=params.learningRate)
+    loss_fn = keras.losses.CategoricalCrossentropy()
+    loss_fn_2 = keras.losses.categorical_crossentropy
+    metricAcc = keras.metrics.CategoricalAccuracy()
+    cateAcc = model.CateAcc()
+
+    datalen = dataloader.trainLen
+    weights = tf.ones([datalen], dtype=tf.float32)
+    timer = utils.Timer()
+    # trainables = [splitModel.conv.trainable_variables,
+    #   splitModel.predict.trainable_variables]
+    for epochID in range(1, params.numEpochs+1):
+        IDcnt = 0
+        batchCnt = 0
+        metricAcc.reset_states()
+        trainloss = 0
+        encode_record = []
+        # loss_record = []
+        for batch, labels in trainSet:
+            batchSize = batch.shape[0]
+            batchWeight = weights[IDcnt: IDcnt+batchSize]
+            with tf.GradientTape() as tape:
+                prediction, encoding = splitModel(batch, training=True)
+                loss = loss_fn(labels, prediction, batchWeight)
+                # loss_vec = loss_fn_2(labels, prediction)
+            trainloss += loss
+            gradient = tape.gradient(loss, splitModel.trainable_variables)
+            optimizer.apply_gradients(
+                zip(gradient, splitModel.trainable_variables))
+            metricAcc.update_state(labels, prediction)
+
+            encode_record.append(encoding.numpy())
+            # loss_record.append(loss_vec.numpy())
+
+            batchCnt += 1
+            IDcnt += batchSize
+        trainAcc = metricAcc.result()
+        trainloss /= batchCnt
+
+        batchCnt = 0
+        testloss = 0
+        metricAcc.reset_states()
+        for batch, labels in testSet:
+            prediction, _ = splitModel(batch, training=False)
+            loss = loss_fn(labels, prediction)
+            testloss += loss
+            batchCnt += 1
+            metricAcc.update_state(labels, prediction)
+        testAcc = metricAcc.result()
+        testloss /= batchCnt
+
+        print(f"Epoch {epochID} {timer()}")
+        print(f"Train Loss: {trainloss} Accuracy: {trainAcc}")
+        print(f"Test Loss: {testloss} Accuracy: {testAcc}")
+
+        if epochID >= params.warmUpEpochs:
+            encode_record = np.concatenate(encode_record)
+            weights = weightUpdateFunc(old_encode_record, encode_record, params)
+
+        old_encode_record = encode_record
+
+    splitModel.save_weights(
+        path.join(params.modelPath, "splitModel.keras"))
+    metricAcc.reset_states()
+    cateAcc.reset_states()
+    for batch, labels in valSet:
+        prediction, _ = splitModel(batch, training=False)
+        metricAcc.update_state(labels, prediction)
+        cateAcc.update_state(labels, prediction)
+    acc = cateAcc.result()
+    print(metricAcc.result())
+    for i in acc:
+        print("%.2lf," % i, end=' ')
+    print()
+    print("%.3lf" % acc[9])
+
+
 if __name__ == "__main__":
     params = option.read()
 
@@ -162,7 +362,9 @@ if __name__ == "__main__":
         "None": lambda x: None,
         "baseline": trainBaseline,
         "weightedBaseline": trainWeightedBaseline,
-        "kmean": lambda x: trainNeighbourWeight(x, KMeanupdate)
+        "kmean": lambda x: trainNeighbourWeight(x, KMeanupdate),
+        "naiveloss": lambda x: trainLossWeight(x, Onlylossupdate),
+        "featureL1": lambda x: trainFeatureWeight(x, FeatureL1update)
     }
     if params.useGPU:
         utils.selectDevice(0)
